e89156c28fb25988b89bf6e3c474896aaa85d731
Huy317 <167590855+Huy317@users.noreply.github.com>
2024-06-22 09:07:24 +0700

    set look and feel to be windows

--- src/main/java/imagemanager/ImageManager.java
+++ src/main/java/imagemanager/ImageManager.java
@@ -5,6 +5,8 @@ package imagemanager;
 
 import Frame.*;
 import imagemanager.*;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -15,7 +17,16 @@ public class ImageManager {
     public static void main(String[] args) {
         Manager manager = Manager.getInstance();
         TagManager tagManager = TagManager.getInstance();
-        
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {
+
+        }
         MainFrame2 frame = new MainFrame2();
         frame.setVisible(true);
 
